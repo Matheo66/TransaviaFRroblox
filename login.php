a67e959a3274cd859779b9d078c95e4a9e54ce85
Matheo66 <ecolomatheo66@gmail.com>
2025-01-12 12:28:18 +0100

    reparer

--- login.php
+++ login.php
@@ -5,7 +5,6 @@ $username = "root";         // ton nom d'utilisateur MySQL
 $password = "";             // ton mot de passe MySQL (vide par défaut sous XAMPP)
 $dbname = "transavia";      // Nom de ta base de données
 
-// Créer la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Vérifier la connexion
@@ -13,11 +12,8 @@ if ($conn->connect_error) {
     die("Connexion échouée : " . $conn->connect_error);
 }
 
-// Démarrer la session si ce n'est pas déjà fait
-session_start();
-
-// Vérifier si les champs du formulaire existent
-if (isset($_POST['username']) && isset($_POST['password'])) {
+// Vérifier si les champs du formulaire existent et que la méthode est POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset($_POST['password'])) {
     // Récupérer les données du formulaire
     $nom_utilisateur = $_POST['username'];
     $mot_de_passe = md5($_POST['password']);  // Hachage du mot de passe avec MD5
@@ -30,7 +26,8 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         // Connexion réussie, récupérer les infos de l'utilisateur
         $row = $result->fetch_assoc();
         
-        // Stocker les informations dans la session
+        // Démarrer la session et stocker les informations
+        session_start();
         $_SESSION['username'] = $nom_utilisateur;
         $_SESSION['nom'] = $row['nom'];
         $_SESSION['prenom'] = $row['prenom'];
@@ -38,7 +35,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
         // Rediriger vers le tableau de bord staff
         header("Location: staff_dashboard.php");
-        exit();  // S'assurer que le script s'arrête après la redirection
+        exit();
     } else {
         echo "Nom d'utilisateur ou mot de passe incorrect.";
     }
